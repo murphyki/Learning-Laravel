fa2eac8fa8e7ca2d91abee7cff5246f6010df38e
Kieran Murphy <kieran.murphy@ie.ibm.com>
2015-09-23 15:46:51 +0100

    Updated show article view

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <h1>View Article Details</h1>
 
-    {!! Form::model($user) !!}
+    {!! Form::model($article) !!}
         <div class="form-group">
             {!! Form::label('title', 'Title:') !!}
             {!! Form::text('title', null, ['class' => 'form-control', 'readonly' => 'true']) !!}
@@ -15,10 +15,10 @@
             {!! Form::textarea('content', null, ['class' => 'form-control', 'readonly' => 'true']) !!}
         </div>
         <div class="btn-group btn-group-lg">
-            <a class="btn btn-info" href="{{ route('user.index') }}">Cancel</a>
-            <a class="btn btn-primary" href="{{ route('user.edit', $user->slug) }}">Edit</a>
+            <a class="btn btn-info" href="{{ route('article.index') }}">Cancel</a>
+            <a class="btn btn-primary" href="{{ route('article.edit', $article->slug) }}">Edit</a>
             @role('super.admin')
-                <a class="btn btn-danger" href="{{ route('user.delete', $user->slug) }}">Delete</a>
+                <a class="btn btn-danger" href="{{ route('article.delete', $article->slug) }}">Delete</a>
             @endrole
         </div>
     {!! Form::close() !!}
