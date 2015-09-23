f77ea4cb4655f984678458934240b94a07e55b2b
Kieran Murphy <kieran.murphy@ie.ibm.com>
2015-09-23 15:53:21 +0100

    Updated show article view

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,25 +1,18 @@
 @extends('app')
 
-@section('title', 'View Article Details')
+@section('title', $article->title)
 
 @section('content')
-    <h1>View Article Details</h1>
+    <h1>{{ $article->title }}</h1>
+    <div>{{ $article->content }}</div>
 
-    {!! Form::model($article) !!}
-        <div class="form-group">
-            {!! Form::label('title', 'Title:') !!}
-            {!! Form::text('title', null, ['class' => 'form-control', 'readonly' => 'true']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('content', 'Content:') !!}
-            {!! Form::textarea('content', null, ['class' => 'form-control', 'readonly' => 'true']) !!}
-        </div>
-        <div class="btn-group btn-group-lg">
-            <a class="btn btn-info" href="{{ route('article.index') }}">Cancel</a>
-            <a class="btn btn-primary" href="{{ route('article.edit', $article->slug) }}">Edit</a>
-            @role('super.admin')
+    @role('super.admin')
+        {!! Form::model($article) !!}
+            <div class="btn-group btn-group-lg">
+                <a class="btn btn-info" href="{{ route('article.index') }}">Cancel</a>
+                <a class="btn btn-primary" href="{{ route('article.edit', $article->slug) }}">Edit</a>
                 <a class="btn btn-danger" href="{{ route('article.delete', $article->slug) }}">Delete</a>
-            @endrole
-        </div>
-    {!! Form::close() !!}
+            </div>
+        {!! Form::close() !!}
+    @endrole
 @endsection
