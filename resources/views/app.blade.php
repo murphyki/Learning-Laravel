082fc5126d44b954593eb9c99b91a6fca5378cf2
Kieran Murphy <kieran.murphy@ie.ibm.com>
2015-09-25 14:45:02 +0100

    Mocking out article sidebar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="/css/app.97419f88.css">
+    <link rel="stylesheet" href="/css/app.b0206d13.css">
 
     <script src="/js/scripts_header.d41d8cd9.js"></script>
     @yield('additional_scripts')
@@ -26,46 +26,47 @@
     your browser</a> to improve your experience.</p>
 <![endif]-->
 
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                    aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Project name</a>
+<div class="container">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                        aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Project name</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="/">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                            <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            @if (Auth::check())
+                                <li><a href="#">{{ Auth::user()->email }}</a></li>
+                                <li class="nav-divider"></li>
+                                <li><a href="{{ route('auth.logout') }}">Log Out</a></li>
+                            @else
+                                <li><a href="{{ route('auth.login') }}">Log In</a></li>
+                            @endif
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="/">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                        <b class="caret"></b>
-                    </a>
-                    <ul class="dropdown-menu">
-                        @if (Auth::check())
-                            <li><a href="#">{{ Auth::user()->email }}</a></li>
-                            <li class="nav-divider"></li>
-                            <li><a href="{{ route('auth.logout') }}">Log Out</a></li>
-                        @else
-                            <li><a href="{{ route('auth.login') }}">Log In</a></li>
-                        @endif
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        <!--/.nav-collapse -->
-    </div>
-</nav>
+    </nav>
+</div>
 
 <div class="container">
     @yield('content')
--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -3,7 +3,29 @@
 @section('title', $article->title)
 
 @section('content')
-    <h1>{{ $article->title }}</h1>
+    <div class="row">
+        <div class="col-sm-8">
+            <h1>{{ $article->title }}</h1>
 
-    <div>{!! $article->content !!}</div>
+            <div>{!! $article->content !!}</div>
+        </div>
+        <div class="col-sm-3 col-sm-offset-1">
+
+            <h1>Latest Articles</h1>
+            <ul>
+                <li>Article 1</li>
+                <li>Article 2</li>
+                <li>Article 3</li>
+                <li>Article 4</li>
+            </ul>
+
+            <h3>Archived Articles</h3>
+            <ul>
+                <li>2014</li>
+                <li>2013</li>
+                <li>2012</li>
+                <li>2011</li>
+            </ul>
+        </div>
+    </div>
 @endsection
